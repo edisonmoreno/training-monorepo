f46ff1f258623d8bb4150de64405e8d01f818ffc
Edison Moreno <john.moreno@sofka.com.co>
2021-10-19 15:52:53 -0500

    Integrar los paquetes

--- packages/react-package/src/App.tsx
+++ packages/react-package/src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const name = require("typescript-package");
+  const variable = name("Edison", "Moreno");
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>{variable}</h2>
       </header>
     </div>
   );
